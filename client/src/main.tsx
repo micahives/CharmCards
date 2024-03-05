a77d6dc57602f1e3ce42f78fdd00d3fb0423664d
Micah Ives <micbenives@gmail.com>
2024-03-05 16:46:14 -0600

    Add autoprefixer for tw css, add error page and react router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { StrictMode } from 'react';
+import './index.css';
+import Error from './pages/Error.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
