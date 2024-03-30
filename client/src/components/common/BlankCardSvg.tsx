5884247a12006299d0a7e7d0c6694ec457a0b1a7
Micah Ives <micbenives@gmail.com>
2024-03-30 15:13:56 -0500

    Optimized card SVGs for react components, removed placeholder from LandingHero

---
+++ client/src/components/common/BlankCardSvg.tsx
@@ -0,0 +1,36 @@
+import { SVGProps } from "react"
+const BlankCardSvg = (props: SVGProps<SVGSVGElement>) => (
+    <svg
+    // Leave viewBox as-is but exclude width/height so SVG fits 100% in parent container
+    // width={295}
+    // height={412}
+    viewBox="0 0 295 412"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g id="blankCard">
+      <g id="frontCard">
+        <path
+          id="frontFace"
+          d="M276.077 35.9193H17.0568V398.94H276.077V35.9193Z"
+          fill="white"
+          stroke="black"
+          strokeWidth={0.979499}
+        />
+        <g id="frontTop">
+          <path
+            d="M259.685 34.0853C259.685 13.5143 258.786 13 258.786 13L17 35.1138"
+            fill="white"
+          />
+          <path
+            d="M259.685 34.0853C259.685 13.5143 258.786 13 258.786 13L17 35.1138"
+            stroke="black"
+            strokeWidth={1.14352}
+          />
+        </g>
+      </g>
+    </g>
+  </svg>
+)
+export { BlankCardSvg }
--- client/src/components/common/LandingHero.tsx
+++ client/src/components/common/LandingHero.tsx
@@ -1,4 +1,5 @@
-import blankCard from '../../assets/blankCard.svg';
+import { BlankCardSvg } from '../common/BlankCardSvg';
+// import { OpenBlankCard } from '../common/OpenBlankCardSvg';
 
 const LandingHero = () => {
     return (
@@ -9,7 +10,6 @@ const LandingHero = () => {
                 {/* column div */}
                 <div className="lg:col-span-3">
                     <h1 className="block text-3xl font-bold text-gray-800 sm:text-4xl md:text-5xl lg:text-6xl dark:text-white">Build custom cards</h1>
-                    <p>hello typescript</p>
                     {/* questionable jsx, may want to look into this functionality of this block */}
                     <div className="mt-5 lg:mt-8 flex flex-col items-center gap-2 sm:flex-row sm:gap-3">
                         <div className="w-full sm:w-auto">
@@ -17,14 +17,14 @@ const LandingHero = () => {
                             <input type="text" id="hero-input" name="hero-input" className="py-3 px-4 block w-full xl:min-w-72 border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Enter work email"></input>
                         </div>
                         <a className="w-full sm:w-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
-                            Request demo
+                            Login
                         </a>
                     </div>
                     {/* end of questionable block */}
                 </div>
                 {/* end of column div */}
-                <div className="lg:col-span-4 mt-10 lg:mt-0">
-                    <img className="w-3/4 rounded-xl" src={blankCard}></img>
+                <div className="lg:col-span-3 mt-10 lg:mt-0">
+                    <BlankCardSvg />
                 </div>
             </div>
             {/* end of grid div */}
