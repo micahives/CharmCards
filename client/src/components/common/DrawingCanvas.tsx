54a54c6b26828c58fe6564ddc28dd4bfa2d1a317
Micah Ives <micbenives@gmail.com>
2024-06-08 18:25:12 -0500

    Create mouse event handlers, update useEffect hook with empty dependency array

--- client/src/components/common/DrawingCanvas.tsx
+++ client/src/components/common/DrawingCanvas.tsx
@@ -1,15 +1,36 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import rough from 'roughjs';
 
 const DrawingCanvas = () => {
+
     useEffect(() => {
-        const rc = rough.canvas(document.getElementById('canvas') as HTMLCanvasElement);
-        rc.rectangle(10, 10, 200, 200); // x, y, width, height
-    })
+        const canvasElement = document.getElementById('canvas') as HTMLCanvasElement;
+        const rc = rough.canvas(canvasElement);
+        rc.rectangle(200, 200, 400, 400); // x, y, width, height
+    }, []) // empty array: dependency array, the effect runs only once after the initial render vs. no array: effect runs after each render
+
+    const handleMouseDown = () => {
+        console.log('Mouse was pressed down');
+    };
+
+    const handleMouseUp = () => {
+        console.log('Mouse was released');
+    };
+
+    const handleMouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
+        console.log('Mouse position:', event.clientX, event.clientY);
+    };
 
     return (
-        <canvas id="canvas" style={{backgroundColor: 'white'}} width={1500} height={innerHeight}>
-
+        <canvas 
+            id="canvas" 
+            style={{backgroundColor: 'white'}} 
+            width={1500} 
+            height={innerHeight}
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+            onMouseMove={handleMouseMove}
+            >Drawing canvas
         </canvas>
     )
 }
